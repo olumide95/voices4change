6d807d27327c772f0a77c147703b43ec4575011e
Olumide <olugbemiro.olumide@gmail.com>
2020-06-14 00:48:30 +0100

    Landing page added

---
+++ src/Views/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import FormBox from "../Components/FormBox";
+
+export default class Home extends Component {
+  render() {
+    return <FormBox />;
+  }
+}
